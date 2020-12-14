9bf0038e9bc5b8b08220b7d56e9d9845ec427a59
ryanliang000 <mail>
2020-12-14 21:31:50 +0800

    update

--- log.h
+++ log.h
@@ -3,9 +3,9 @@
 #define LOG(fd, title, ...)\
 	fprintf(fd, title);\
 	fprintf(fd, __VA_ARGS__);\
-	fprintf(fd, " -line:%d\n", __LINE__);
+	fprintf(fd, " (line:%d)\n", __LINE__);
 
-#ifdef RELEASE
+#ifdef REL
 	#define LOG_E(...) LOG(stderr, "[ERR]", __VA_ARGS__)
 	#define LOG_R(...)
 	#define LOG_I(...)
@@ -16,7 +16,11 @@
 #else
 	#define LOG_E(...) LOG(stderr, "[ERR]", __VA_ARGS__)
 	#define LOG_R(...) LOG(stdout, "[RUN]", __VA_ARGS__) 
-	#define LOG_I(...) {LOG(stdout, "[INF]", __VA_ARGS__);}
+	#define LOG_I(...) LOG(stdout, "[INF]", __VA_ARGS__)
 #endif
 
-
+#ifdef DBG
+	#define LOG_D(...) LOG(stdout, "[DBG]", __VA_ARGS__)
+#else
+	#define LOG_D(...)
+#endif
